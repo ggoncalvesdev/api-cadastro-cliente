4c6c349d9f491ba668905b6ebfae818c2ca2aa0b
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2022-10-21 11:30:57 -0300

    feat(Configuration): Project SpringBoot

---
+++ api-cadastro-cliente/src/main/java/com/api/cadastro_cliente/apicadastrocliente/ApiCadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package com.api.cadastro_cliente.apicadastrocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroClienteApplication.class, args);
+	}
+
+}
---
+++ api-cadastro-cliente/src/test/java/com/api/cadastro_cliente/apicadastrocliente/ApiCadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cadastro_cliente.apicadastrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
