e78d92a807199fe3a66deb9e0ec65c619ba4f15a
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2022-10-21 11:34:15 -0300

    fix(Correction): Root Folder

--- api-cadastro-cliente/src/main/java/com/api/cadastro_cliente/ApiCadastroClienteApplication.java
+++ api-cadastro-cliente/src/main/java/com/api/cadastro_cliente/ApiCadastroClienteApplication.java
@@ -1,4 +1,4 @@
-package com.api.cadastro_cliente.apicadastrocliente;
+package com.api.cadastro_cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiCadastroClienteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiCadastroClienteApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ApiCadastroClienteApplication.class, args);
+  }
 }
