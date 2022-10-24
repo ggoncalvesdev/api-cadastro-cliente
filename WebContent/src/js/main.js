0207cce200315ad715eaae9b20331c8f200a8bda
pedrowiskutzki <wiskutzki@gmail.com>
2022-10-24 15:07:12 -0300

    feat(Notes):ImplemntNoteApi

--- WebContent/src/js/main.js
+++ WebContent/src/js/main.js
@@ -1,22 +1,29 @@
+//Apontando documento HTML: Campo texto "cep"
 const cep = document.querySelector("#cep")
 
+//Tratando o resultado
 const showData = (result)=>{
-    for(const campo in result){
-        if(document.querySelector("#"+campo)){
-            document.querySelector("#"+campo).value = result[campo]
+    for(const campo in result){ //Pra cada um dos resultados armazena ele no campo
+        if(document.querySelector("#"+campo)){ // Pegue nesse docuemnto pelo id se não descate
+            document.querySelector("#"+campo).value = result[campo] //
+            // Se existir o id escreva 
         }
     }
 }
+//Evento                        Arrow
 cep.addEventListener("blur", (e)=>{
     let search = cep.value.replace("-","")
-    const options = {
+    const options = { //Parametros
         method: 'GET',
-        mode: 'cors',
+        mode: 'cors', 
         cache: 'default'
-    }
+    }                                   //ADD variavel search     
     fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    .then(response=>{ response.json()
+        //Se der certo
+        .then(response=>{ response.json()
+        //Trate o resultado    
         .then(data => showData(data))
     })
+    //Se der errado
     .catch(e => console.log('Deu Erro: '+ e,message))
 })
